ee50fd9babe74432fd49d2175a227074352ea5bb
Karan Thakur <thakurkaran0345@gmail.com>
2024-11-17 12:22:55 +0530

    7

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,17 @@
-import { auth } from '@/auth'
+import { auth, signOut } from '@/auth'
+import { Button } from '@/components/ui/Button';
 
 const settings = async () => {
     const session = await auth();
     return (
         <div>
             <h1>{JSON.stringify(session)}</h1>
+            <form action={async() => {
+                "use server"
+                await signOut({ redirect: false });
+            }}>
+                <Button type="submit">Logout</Button>
+            </form>
         </div>
     );
 }
